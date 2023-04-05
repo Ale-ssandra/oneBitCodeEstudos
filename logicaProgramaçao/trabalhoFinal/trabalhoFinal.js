6b7b42fe1eed7a438d43266a28e5b9be20de73b6
Ale-ssandra <lele.p.cunha@gmail.com>
2023-04-04 23:08:05 -0300

    criaçao de vagas

--- logicaProgramaçao/trabalhoFinal/trabalhoFinal.js
+++ logicaProgramaçao/trabalhoFinal/trabalhoFinal.js
@@ -1,35 +1,34 @@
 let menu;
-let novaVaga = {}
-let vagas = []
+let vagas = [];
 do {
   menu = prompt(
     "1- Mostrar vagas disponiveis \n 2- Criar nova vaga \n 3- Visualizar uma vaga \n 4- Candidatar-se a uma vaga \n 5- Excluir vaga \n 6- Sair"
   );
-    switch (menu) {
-        case "1":
-         novaVaga = prompt("Qual seu nome")  
-         vagas.push(novaVaga)
-         console.log(vagas)
-        case "2":
-    
-            break;
-        case "3":
-            
-            break;
-        case "4":
-            
-            break;
-        case "5":
-            
-            break;
-        case "6":
-            alert("Saindo!")
-            
-            break;
-    
-        default:
-            break;
-    }
+  switch (menu) {
+    case "1":
 
+    case "2":
+        let novaVaga = { nomeVaga: "", descricao: "", data: "" };
+        novaVaga.nomeVaga = prompt("Qual nome da vaga?");
+        novaVaga.descricao = prompt("Fale um pouco da vaga:");
+        novaVaga.data = prompt("Qual data limite: xx-xx-xxxx");
+        vagas.push(novaVaga);
+        console.log(vagas);
+    
+        
+      break;
+    case "3":
+      break;
+    case "4":
+      break;
+    case "5":
+      break;
+    case "6":
+      alert("Saindo!");
 
+      break;
+
+    default:
+      break;
+  }
 } while (menu !== "6");
